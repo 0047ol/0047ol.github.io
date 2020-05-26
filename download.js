ae2f0001bcf533756c8bd76f44cf3c867cccaaac
0047ol <2088343717@qq.com>
2020-05-26 23:24:09 +0800

    Create download.js

---
+++ download.js
@@ -0,0 +1,84 @@
+//Photoshop
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/cn/ccpp-related/Adobe_Photoshop_logo_RGB.svg", "Adobe Photoshop", "Win/Mac", "简体中文", "21.1.2.136", "集成破解", "2020-04-15", "TitlePs", "TooltipPs", "listPs");
+addPanelList("listPs", "Adobe Photoshop CC 2020", "https://cloud189-henan-person.oos-hazz.ctyunapi.cn/1306ad8b-9aa6-481e-bbc6-02e768a818a0?x-amz-UFID=8151228760771254&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1866888181&Expires=1590499858&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Photoshop_2020_21.1.3.190_ACR12.2.1_SP_20200518.rar%22&AWSAccessKeyId=532482dfdc85e5d60592&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=zojPH87LIo%2BTDWZAMX2tv4x6K9Y%3D", "", "MAC", "", "ps20");
+addPanelList("listPs", "Adobe Photoshop CC 2019", "https://cloud189-shandong-person.oos-sdqd.ctyunapi.cn/b1f0630e-1da8-4117-b6a3-3d7281af9b66?x-amz-UFID=1131137414383895&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1905346326&Expires=1590496769&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Photoshop_CC_2019_20.0.9.113_ACR12.1_SP_20200329.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=nxWViWHhvKHQF6l2bhnY/BuSUKk%3D", "", "MAC", "", "ps19");
+addPanelList("listPs", "Adobe Photoshop CC 2018", "WIN", "", "MAC", "", "ps18");
+addPanelList("listPs", "Adobe Photoshop CC 2017", "WIN", "", "MAC", "", "ps17");
+addPanelList("listPs", "Adobe Photoshop CS6", "WIN", "", "MAC", "", "ps6");
+//Illustrator
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/illustrator.svg", "Adobe Illustrator", "Win/Mac", "简体中文", "24.1.2.408", "集成破解", "2020-04-16", "TitleAi", "TooltipAi", "listAi");
+addPanelList("listAi", "Adobe Illustrator CC 2020", "https://cloud189-anhui-person.oos-ahwh.ctyunapi.cn/c5f52e6e-2256-41af-bb81-a0eb5db33296?x-amz-UFID=6132227595060501&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1404840067&Expires=1590509111&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Illustrator_2020_24.1.2.408_SP_20200416.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=acHySFTUqBKZKytj%2BbTSkQs1NMQ%3D", "", "MAC", "", "ai20");
+addPanelList("listAi", "Adobe Illustrator CC 2019", "WIN", "", "MAC", "", "ai19");
+addPanelList("listAi", "Adobe Illustrator CC 2018", "WIN", "", "MAC", "", "ai18");
+addPanelList("listAi", "Adobe Illustrator CC 2017", "WIN", "", "MAC", "", "ai17");
+addPanelList("listAi", "Adobe Illustrator CS6", "WIN", "", "MAC", "", "ai6");
+//After Effects
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/aftereffects.svg", "After Effects", "Win/Mac", "简体中文", "17.0.6.35", "集成破解", "2020-04-14", "TitleAe", "TooltipAe", "listAe");
+addPanelList("listAe", "After Effects CC 2020", "https://cloud189-henan-person.oos-hazz.ctyunapi.cn/238fa3d7-20c8-4495-b305-b823e8974e10?x-amz-UFID=8150328757752447&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=2030799562&Expires=1590513864&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_After_Effects_2020_17.1.0.72_ACR12.2.1_SP_20200518.rar%22&AWSAccessKeyId=532482dfdc85e5d60592&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=E3/GZcSudSAoXc%2BUvdqDib2ezX8%3D", "", "MAC", "", "ae20");
+addPanelList("listAe", "After Effects CC 2019", "WIN", "", "MAC", "", "ae19");
+addPanelList("listAe", "After Effects CC 2018", "WIN", "", "MAC", "", "ae18");
+addPanelList("listAe", "After Effects CC 2017", "WIN", "", "MAC", "", "ae17");
+addPanelList("listAe", "After Effects CS6", "WIN", "", "MAC", "", "ae6");
+//Premiere
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/pr_cc_app_RGB.svg", "Adobe Premiere Pro", "Win/Mac", "简体中文", "14.1.0.116", "集成破解", "2020-04-15", "TitlePr", "TooltipPr", "listPr");
+addPanelList("listPr", "Adobe Premiere Pro CC 2020", "https://cloud189-anhui-person.oos-ahwh.ctyunapi.cn/c2d2152c-7ca7-44c5-b2ce-817c0ebee8f2?x-amz-UFID=5150328816074457&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1567559521&Expires=1590510500&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Premiere_Pro_2020_14.2.0.47_SP_20200518.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=byhNhXhLfiXEiHBu/RUciEfW/zs%3D", "", "MAC", "", "pr20");
+addPanelList("listPr", "Adobe Premiere Pro CC 2019", "WIN", "", "MAC", "", "pr19");
+addPanelList("listPr", "Adobe Premiere Pro CC 2018", "WIN", "", "MAC", "", "pr18");
+addPanelList("listPr", "Adobe Premiere Pro CC 2017", "WIN", "", "MAC", "", "pr17");
+addPanelList("listPr", "Adobe Premiere Pro CS6", "WIN", "", "MAC", "", "pr6");
+//Dreamweaver
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/dw_cc_app_RGB.svg", "Adobe Dreamweaver", "Win/Mac", "简体中文", "20.1.0.15211", "集成破解", "2020-02-12", "TitleDw", "TooltipDw", "listDw");
+addPanelList("listDw", "Adobe Dreamweaver CC 2020", "https://cloud189-anhui-person.oos-ahwh.ctyunapi.cn/c5ff4d0a-2392-4ba7-9b90-eb3a3e9ab38d.rar?x-amz-UFID=1131137414383711&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1101537082&Expires=1590506873&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Dreamweaver_2020_20.1.0.15211_SP_20200212.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=1kQtOSAy65N0PWhcmle7vlNsn7Y%3D", "", "MAC", "", "dw20");
+addPanelList("listDw", "Adobe Dreamweaver CC 2019", "WIN", "", "MAC", "", "dw19");
+addPanelList("listDw", "Adobe Dreamweaver CC 2018", "WIN", "", "MAC", "", "dw18");
+addPanelList("listDw", "Adobe Dreamweaver CC 2017", "WIN", "", "MAC", "", "dw17");
+addPanelList("listDw", "Adobe Dreamweaver CS6", "WIN", "", "MAC", "", "dw6");
+//Audition
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/audition.svg", "Adobe Audition", "Win/Mac", "简体中文", "13.0.5.36", "集成破解", "2020-04-14", "TitleAu", "TooltipAu", "listAu");
+addPanelList("listAu", "Adobe Audition CC 2020", "https://cloud189-henan-person.oos-hazz.ctyunapi.cn/89197dc6-4d01-4575-925b-ea1675a7c5c3?x-amz-UFID=9154128804202033&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=414852047&Expires=1590501659&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Audition_2020_13.0.6.38_SP_20200518.rar%22&AWSAccessKeyId=532482dfdc85e5d60592&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=PSndWF2yltIhyjqlDnc3W2/ZCxU%3D", "", "MAC", "", "au20");
+addPanelList("listAu", "Adobe Audition CC 2019", "WIN", "", "MAC", "", "au19");
+addPanelList("listAu", "Adobe Audition CC 2018", "WIN", "", "MAC", "", "au18");
+addPanelList("listAu", "Adobe Audition CC 2017", "WIN", "", "MAC", "", "au17");
+addPanelList("listAu", "Adobe Audition CS6 v", "WIN", "", "MAC", "", "au6");
+//Lightroom
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/icons/lightroom-classic.svg", "Adobe Lightroom Classic", "Win/Mac", "简体中文", "9.2.1.202004070813", "集成破解", "2020-04-14", "TitleLr", "TooltipLr", "listLr");
+addPanelList("listLr", "Adobe Lightroom Classic CC 2020", "https://cloud189-shandong-person.oos-sdqd.ctyunapi.cn/2ee98510-484e-4480-a9b9-405cb4bc68c1?x-amz-UFID=6130127112421340&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=1084988880&Expires=1590506673&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Lightroom_Classic_9.2.1.202004070813_SP_20200414.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=v0zckOL69C9cUOVutvR8zluudMs%3D", "", "MAC", "", "lr20");
+addPanelList("listLr", "Adobe Lightroom Classic CC 2019", "WIN", "", "MAC", "", "lr19");
+addPanelList("listLr", "Adobe Lightroom Classic CC 2018", "WIN", "", "MAC", "", "lr18");
+//Media Encoder
+addPanelItem("tab-content-adobe", "https://www.adobe.com/content/dam/cc/us/en/products/media-encoder/media-encoder.svg", "Adobe Media Encoder", "Win/Mac", "简体中文", "14.1.0.155", "集成破解", "2020-04-14", "TitleMe", "TooltipMe", "listMe");
+addPanelList("listMe", "Adobe Media Encoder CC 2020", "WIN", "", "https://cloud189-anhui-person.oos-ahwh.ctyunapi.cn/edf23e8f-ae24-46b2-808c-71de5651da88?x-amz-UFID=2151228798072289&x-amz-CLIENTNETWORK=UNKNOWN&x-amz-SHID=169098942&x-amz-FSIZE=939375020&Expires=1590505658&x-amz-UID=262174363&response-content-disposition=attachment%3Bfilename%3D%22Adobe_Media_Encoder_2020_14.2.0.45_SP_20200518.rar%22&AWSAccessKeyId=caf5e6901807aca55a45&x-amz-limitrate=61440&x-amz-CLOUDTYPEIN=PERSON&x-amz-CLIENTTYPEIN=WEB&Signature=TMoMoGdgPJltzkjomIDA/wYYfq4%3D", "", "me20");
+addPanelList("listMe", "Adobe Media Encoder CC 2019", "WIN", "", "MAC", "", "me19");
+addPanelList("listMe", "Adobe Media Encoder CC 2018", "WIN", "", "MAC", "", "me18");
+addPanelList("listMe", "Adobe Media Encoder CC 2017", "WIN", "", "MAC", "", "me17");
+addPanelList("listMe", "Adobe Media Encoder CS6", "WIN", "", "MAC", "", "me6");
+//会声会影
+addPanelItem("tab-content-corel", "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=70304127,2974181107&amp;fm=11&amp;gp=0.jpg", "Corel VideoStudio Pro", "Windows", "简体中文", "23.1.0.482", "手动破解", "2020-03-31", "TitleHshy", "TooltipHshy", "listHshy");
+addPanelList("listHshy", "Corel VideoStudio Pro 2020", "https://pan.masuit.com/%E4%BC%9A%E5%A3%B0%E4%BC%9A%E5%BD%B1/?rootId=0ABNyUlpZBcXVUk9PVA", "", "", "", "hshy20");
+addPanelList("listHshy", "Corel VideoStudio Pro 2019", "https://node4.mkdmirror.workers.dev/VideoStudio/videostudio.zip", "", "", "", "hshy19");
+addPanelList("listHshy", "Corel VideoStudio Pro 2018", "https://odobagg-my.sharepoint.com/personal/ph4fpnnul8_od_obagg_com/_layouts/15/onedrive.aspx?originalPath=aHR0cHM6Ly9vZG9iYWdnLW15LnNoYXJlcG9pbnQuY29tLzpmOi9nL3BlcnNvbmFsL3BoNGZwbm51bDhfb2Rfb2JhZ2dfY29tL0V1bUFfc1RSRXpWRHBpVmJjN2xiU3ZnQmpYb0pBMkpzeGZEVGZoY3MwQl9aUFE_cnRpbWU9TGFLRUtXY0IyRWc&id=%2Fpersonal%2Fph4fpnnul8_od_obagg_com%2FDocuments%2F%E4%BC%9A%E5%A3%B0%E4%BC%9A%E5%BD%B1%2F2018", "", "", "", "hshy18");
+addPanelList("listHshy", "Corel VideoStudio Pro X10", "WIN", "", "", "", "hshy10");
+addPanelList("listHshy", "Corel VideoStudio Pro X9", "WIN", "", "", "", "hshy9");
+addPanelList("listHshy", "Corel VideoStudio Pro X8", "WIN", "", "", "", "hshy8");
+//达芬奇
+addPanelItem("tab-content-bmd", "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=942019405,3671026692&fm=26&gp=0.jpg", "DaVinci Resolve Studio", "Win/Mac", "简体中文", "16.2.2.11", "手动破解", "2020-05-20", "TitleDfq", "TooltipDfq", "listDfq");
+addPanelList("listDfq", "DaVinci Resolve Studio v16", "WIN", "", "MAC", "", "dfq16");
+addPanelList("listDfq", "DaVinci Resolve Studio v15", "WIN", "", "MAC", "", "dfq15");
+addPanelList("listDfq", "DaVinci Resolve Studio v14", "WIN", "", "MAC", "", "dfq14");
+addPanelList("listDfq", "DaVinci Resolve Studio v13", "WIN", "", "MAC", "", "dfq13");
+addPanelList("listDfq", "DaVinci Resolve Studio v12", "WIN", "", "MAC", "", "dfq12");
+addPanelList("listDfq", "DaVinci Resolve Studio v11", "WIN", "", "MAC", "", "dfq11");
+addPanelList("listDfq", "DaVinci Resolve Studio v10", "WIN", "", "MAC", "", "dfq10");
+//Vegas
+addPanelItem("tab-content-magix", "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1450597258,1630441204&fm=26&gp=0.jpg", "Magix Vegas Pro", "Windows", "简体中文", "17.0.0.421", "手动破解", "2020-02-20", "TitleVv", "TooltipVv", "listVv");
+addPanelList("listVv", "Magix Vegas Pro v17", "https://node4.mkdmirror.workers.dev/VegasPro/vegaspro17.zip", "", "", "", "vv17");
+addPanelList("listVv", "Magix Vegas Pro v16", "https://node4.mkdmirror.workers.dev/VegasPro/vegaspro16.zip", "", "", "", "vv16");
+addPanelList("listVv", "Magix Vegas Pro v15", "WIN", "", "", "", "vv15");
+addPanelList("listVv", "Magix Vegas Pro v14", "WIN", "", "", "", "vv14");
+addPanelList("listVv", "Magix Vegas Pro v13", "WIN", "", "", "", "vv13");
+//Edius
+addPanelItem("tab-content-grass", "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3772118960,2431841205&fm=26&gp=0.jpg", "Grass Valley Edius Pro", "Windows", "简体中文", "9.20.3340", "手动破解", "2018-12-09", "TitleEd", "TooltipEd", "listEd");
+addPanelList("listEd", "Grass Valley Edius Pro v9", "https://node4.mkdmirror.workers.dev/Edius9/crack.zip", "", "", "", "ed9");
+addPanelList("listEd", "Grass Valley Edius Pro v8", "WIN", "", "", "", "ed8");
+addPanelList("listEd", "Grass Valley Edius Pro v7", "WIN", "", "", "", "ed7");
+addPanelList("listEd", "Grass Valley Edius Pro v6", "WIN", "", "", "", "ed6");
